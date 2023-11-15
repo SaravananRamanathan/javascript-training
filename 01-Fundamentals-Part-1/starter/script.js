d9d4ee73df73cdef781d23faa1d3bead45c48581
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2023-11-15 10:57:00 +0530

    part 1 changes

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,109 @@
+// let js = 'amazing';
+
+// console.log(40+8+23-10);
+// console.log('srvnn')
+// console.log(23);
+
+// let firstName = 'Saravanan'
+// console.log(firstName)
+// console.log(firstName)
+// console.log(firstName)
+
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun)
+
+// console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Srvnn');
+
+// javaScriptIsFun = 'YES!'
+
+// console.log(typeof null)
+
+// console.log(null)
+
+// const ageSrvnn = 2037 - 1996;
+// const ageTada = 2023 - 1996;
+
+// console.log(ageSrvnn, ageTada)
+
+// let x = 100
+
+// console.log(x--)
+
+// console.log(x)
+
+// const firstNmae = "srvnn";
+// const job = 'author';
+// const birthYear = 1996;
+// const year = 2023;
+
+// const srvnn = "I'm " + firstNmae + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+
+// console.log(srvnn)
+
+// const srvnnNew = `I'm ${firstNmae}`;
+// console.log(srvnnNew)
+
+// const age = 15;
+
+// if (age >= 18) {
+//     console.log("tada can start drining license 🚗")
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`tada you are too young, wait another ${yearsLeft} years.`)
+// }
+
+// const hasDriversLicense = true;
+// const hasGoodVision = false;
+
+// console.log(hasDriversLicense && hasGoodVision)
+// console.log(hasDriversLicense || hasGoodVision)
+// console.log(!hasDriversLicense)
+
+// const day = 'sunday';
+
+// switch (day) {
+//     case 'monday':
+//         console.log('Plan course structure')
+//         console.log('Go to coding meetup')
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare thory videos')
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('Write code examples')
+//         break;
+//     case 'friday':
+//         console.log('Record videos')
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('Enjoy the weekend :D')
+//         break;
+//     default:
+//         console.log('Not a valid day!')
+//         break;
+// }
+
+// console.log('--------')
+
+// if (day === 'monday') {
+//     console.log('Plan course structure')
+//     console.log('Go to coding meetup')
+// } else if (day === 'tuesday') {
+//     console.log('Prepare thory videos')
+// } else if (day === 'wednesday' || day === 'thursday') {
+//     console.log('Write code examples')
+// } else if (day === 'friday') {
+//     console.log('Record videos')
+// } else if (day === 'saturday' || day === 'sunday') {
+//     console.log('Enjoy the weekend :D')
+// } else {
+//     console.log('Not a valid day!')
+// }
+
+const age = 17;
+age >= 18 ? console.log("I like to drink wine 🍷") : console.log('I linke to drnink water 💧')
