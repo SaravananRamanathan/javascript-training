009c21b22fa08a613ac0f2b29bc3d0f2fdb483b1
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2023-11-17 15:04:09 +0530

    guess_my_num_game: manipulating css from js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -20,7 +20,7 @@
 
 //secret num:
 const secretNumber = Math.trunc(Math.random() * 20);
-document.querySelector('.number').textContent = secretNumber;
+// document.querySelector('.number').textContent = secretNumber;
 
 //score:
 let score = 20;
@@ -29,10 +29,16 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   if (!guess) {
+    // when no input
     document.querySelector('.message').textContent = '⛔️ No Number.';
   } else if (guess === secretNumber) {
+    // when players wins
     document.querySelector('.message').textContent = '🎉 Correct number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
+    // when guess is too high
     if (score > 1) {
         score -= 1;
         document.querySelector('.message').textContent = '📈 Too High!'
@@ -42,6 +48,7 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('.score').textContent = 0;
     }
   } else if (guess < secretNumber) {
+    // when guess is too low
     if (score > 1) {
         score -= 1;
         document.querySelector('.message').textContent = '📉 Too Low!'
