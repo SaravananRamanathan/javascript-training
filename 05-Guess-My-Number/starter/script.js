79aa63ea467628b5ebf06c39287c53f8d36fcf0b
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2023-11-17 15:38:33 +0530

    guess_my_num_game: implemented high score logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -24,6 +24,7 @@ let secretNumber = Math.trunc(Math.random() * 20);
 
 //score:
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -37,6 +38,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+        highScore = score;
+        document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     // when guess is too high
     if (score > 1) {
