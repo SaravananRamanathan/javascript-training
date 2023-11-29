bce5198889283b84b7d4f03615309c9840ace7e4
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2023-11-29 14:40:00 +0530

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,6 +11,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
 
   openingHours: {
     thu: {
@@ -27,3 +30,34 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr)
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+//swapping values using array destructuring
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// receive 2 return values from a function:
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// nested destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, ,j] = nested;
+// console.log(i, j);
+const [i, ,[j, k]] = nested;
+console.log(i, j, k);
+
+//default values:
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
