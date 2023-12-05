8eef201d0891f69509ea70f05e699fb53f5b67f0
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2023-12-05 11:40:57 +0530

    Nullish operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -53,7 +53,13 @@ const restaurant = {
   }
 };
 
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
 
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
 
 
 // // SPREAD, because on right side of =
