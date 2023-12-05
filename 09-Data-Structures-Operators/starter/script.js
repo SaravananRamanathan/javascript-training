ecd5353bad5a908025bf07b6313cf156ad9a739b
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2023-12-05 09:43:20 +0530

    starting destructuring objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -30,34 +30,36 @@ const restaurant = {
     },
   },
 };
+// destructuring objects
 
-const arr = [2,3,4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+// destructuring arrays
+// const arr = [2,3,4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
 
-const [x, y, z] = arr;
-console.log(x, y, z);
-console.log(arr)
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(arr)
 
-let [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
 
-//swapping values using array destructuring
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
+// //swapping values using array destructuring
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
 
-// receive 2 return values from a function:
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
+// // receive 2 return values from a function:
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
 
-// nested destructuring
-const nested = [2, 4, [5, 6]];
-// const [i, ,j] = nested;
-// console.log(i, j);
-const [i, ,[j, k]] = nested;
-console.log(i, j, k);
+// // nested destructuring
+// const nested = [2, 4, [5, 6]];
+// // const [i, ,j] = nested;
+// // console.log(i, j);
+// const [i, ,[j, k]] = nested;
+// console.log(i, j, k);
 
-//default values:
-const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r);
+// //default values:
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r);
