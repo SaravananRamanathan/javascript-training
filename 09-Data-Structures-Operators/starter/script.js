d265ae95f16be6d122287d1ab2a9af6b08ff1cd6
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2023-12-05 10:10:25 +0530

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,37 +54,37 @@ restaurant.orderDelivery({
   starterIndex: 1,
 });
 
-// destructuring objects
-console.log(':testing');
-const { name, openingHours, categories } = restaurant;
-// console.log(categories);
-// console.log(typeof(categories))
-// console.log(restaurant.categories);
+// // destructuring objects
+// console.log(':testing');
+// const { name, openingHours, categories } = restaurant;
+// // console.log(categories);
+// // console.log(typeof(categories))
+// // console.log(restaurant.categories);
 
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(restaurantName, hours, tags);
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantName, hours, tags);
 
-// Default values
-const { menu = [], starterMenu: starters = [] } = restaurant;
-console.log(menu);
-console.log(starters);
+// // Default values
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu);
+// console.log(starters);
 
-// Mutating variables
-let a = 111;
-let b = 999;
-const obj = { a: 23, b: 7, c: 14 };
-({ a, b } = obj);
-console.log(a, b);
+// // Mutating variables
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+// ({ a, b } = obj);
+// console.log(a, b);
 
-// Nested objects
-const {
-  fri: { open: o, close: c },
-} = openingHours;
-console.log(o, c);
+// // Nested objects
+// const {
+//   fri: { open: o, close: c },
+// } = openingHours;
+// console.log(o, c);
 
 // destructuring arrays
 // const arr = [2,3,4];
