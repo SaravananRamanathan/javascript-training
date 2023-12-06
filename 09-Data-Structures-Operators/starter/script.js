81f0822e52d5dc945a4ed97f8eafb4d38519ab7e
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2023-12-06 10:13:22 +0530

    Coding Challenge #2 setup

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -59,62 +59,30 @@ const restaurant = {
   }
 };
 
-// Property NAMES
-const properties = Object.keys(openingHours)
-console.log(properties);
-let openStr = `We are open on ${properties.length} days: `;
 
-for (const [index, day] of properties.entries()) {
-  openStr += `${day}${index<properties.length-1 ? ',' : ''} `;
-}
-console.log(openStr);
+///////////////////////////////////////
+// Coding Challenge #2
 
-// Property VALUES
-const values = Object.values(openingHours);
-console.log(values);
+/* 
+Let's continue with our football betting app!
 
-// Entire object
-const entries = Object.entries(openingHours);
-console.log(...entries.entries());
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
 
-// looping over entires object
-for (const [key, {open, close}] of entries) {
-  console.log(`On ${key} we open at ${open} and close at ${close}`);
-}
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
 
-console.log(`We are open on the following ${Object.keys(openingHours).length} days:`);
-for (const [index, [key, {open, close}]] of Object.entries(openingHours).entries()) {
-  console.log(`${index+1}: On ${key} we open at ${open} and close at ${close}`);
-}
-
-
-// console.log(restaurant.openingHours?.mon?.open);
-
-// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
-// for (const day of days) {
-//   open = restaurant.openingHours[day]?.open ?? 'N/A';
-//   console.log(`On ${day}, we open at ${open}`);
-// }
-
-// // Methods
-// console.log(restaurant.order?.(0,1) ?? 'Method does not exists')
-// console.log(restaurant.orderRisotto?.(0,1) ?? 'Method does not exists')
-
-
-// // Arrays
-// const users = [
-//   {name: 'srvnn', email: 'asdf@example.com'},
-//   {name: 'name2', email: 'name2@example.com'},
-// ]
-// console.log(users[0]?.name ?? `User not found.`);
-
-// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-// for (const [index, item] of menu.entries()) {
-//   console.log(`${index+1}: ${item}`);
-// }
-
-// console.log([...menu.entries()])
-
+GOOD LUCK 😀
+*/
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -154,13 +122,111 @@ const game = {
     x: 3.25,
     team2: 6.5,
   },
-  printGoals: function(...players) {
-    players.forEach(player => {
-      console.log(`${player}: ${players?.length}`)
-    })
-    console.log(`${players?.length} goals were scored`)
-  }
 };
+
+
+// Property NAMES
+// const properties = Object.keys(openingHours)
+// console.log(properties);
+// let openStr = `We are open on ${properties.length} days: `;
+
+// for (const [index, day] of properties.entries()) {
+//   openStr += `${day}${index<properties.length-1 ? ',' : ''} `;
+// }
+// console.log(openStr);
+
+// // Property VALUES
+// const values = Object.values(openingHours);
+// console.log(values);
+
+// // Entire object
+// const entries = Object.entries(openingHours);
+// console.log(...entries.entries());
+
+// // looping over entires object
+// for (const [key, {open, close}] of entries) {
+//   console.log(`On ${key} we open at ${open} and close at ${close}`);
+// }
+
+// console.log(`We are open on the following ${Object.keys(openingHours).length} days:`);
+// for (const [index, [key, {open, close}]] of Object.entries(openingHours).entries()) {
+//   console.log(`${index+1}: On ${key} we open at ${open} and close at ${close}`);
+// }
+
+
+// console.log(restaurant.openingHours?.mon?.open);
+
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+// for (const day of days) {
+//   open = restaurant.openingHours[day]?.open ?? 'N/A';
+//   console.log(`On ${day}, we open at ${open}`);
+// }
+
+// // Methods
+// console.log(restaurant.order?.(0,1) ?? 'Method does not exists')
+// console.log(restaurant.orderRisotto?.(0,1) ?? 'Method does not exists')
+
+
+// // Arrays
+// const users = [
+//   {name: 'srvnn', email: 'asdf@example.com'},
+//   {name: 'name2', email: 'name2@example.com'},
+// ]
+// console.log(users[0]?.name ?? `User not found.`);
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// for (const [index, item] of menu.entries()) {
+//   console.log(`${index+1}: ${item}`);
+// }
+
+// console.log([...menu.entries()])
+
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+//   printGoals: function(...players) {
+//     players.forEach(player => {
+//       console.log(`${player}: ${players?.length}`)
+//     })
+//     console.log(`${players?.length} goals were scored`)
+//   }
+// };
 ///////////////////////////////////////
 // Coding Challenge #1
 
