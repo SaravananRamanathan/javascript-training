772d01a90708e2e390540deaa4b8b6a11ef4fd5c
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2023-12-19 10:11:14 +0530

    commented out maps section

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -59,36 +59,36 @@ const restaurant = {
   },
 };
 
-// Maps
-const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze, Italy');
-console.log(rest.set(2, 'Lisbon, Portugal'));
-rest
-  .set('categories', restaurant.categories)
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We Are open :D')
-  .set(false, 'We are close :(');
+// // Maps
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
+// rest
+//   .set('categories', restaurant.categories)
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We Are open :D')
+//   .set(false, 'We are close :(');
 
-console.log(rest.get('name'));
-console.log(rest.get(true));
-console.log(rest.get(1));
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
 
-const time = 21;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
 
-console.log(rest.has('categories'));
-rest.delete(2);
-// rest.clear()
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// // rest.clear()
 
-const arr = [1,2];
-rest.set(arr, 'Test');
+// const arr = [1,2];
+// rest.set(arr, 'Test');
 
-console.log(rest);
-console.log(rest.size);
+// console.log(rest);
+// console.log(rest.size);
 
-console.log(rest.get(arr));
+// console.log(rest.get(arr));
 
 // // sets
 // const ordersSet = new Set([
