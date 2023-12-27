6a8f30d9e0c7da07df8ca91036718219d9e1cffe
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2023-12-27 10:00:43 +0530

    challenge 4 complete

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -60,6 +60,78 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+/**
+ * Returns the text in cammel case format.
+ * @param {String} text
+ */
+const getFormattedText = function (...texts) {
+  let formattedText = texts.shift().trim();
+  for (let text of texts) {
+    text = text.trim();
+    formattedText += text.toLowerCase().replace(/^./, text[0].toUpperCase());
+  }
+  return formattedText;
+};
+
+const buttonOnClick = function ({ bypassInputText = '' }) {
+  console.log('button clicked...');
+  const inputText = bypassInputText ? bypassInputText : document.querySelector('textarea').value;
+  let output = '';
+  const seperatedTexts = inputText.split('\n');
+  const maxTextLen = Math.max(...seperatedTexts.map(item => item.length));
+  for (const [index, text] of seperatedTexts.entries()) {
+    output += `${getFormattedText(...text.split('_'))
+      .padEnd(maxTextLen + 5)
+      .padEnd(maxTextLen + index + 6, '✅')}\n`;
+  }
+  document.querySelector('textarea').value = output;
+};
+
+document.querySelector('button').addEventListener('click', buttonOnClick);
+
+// buttonOnClick({
+//   bypassInputText: `underscore_case
+//   first_name
+//  Some_Variable 
+//    calculate_AGE
+//  delayed_departure`,
+// });
+
+///////////////////////////////////////
 // Coding Challenge #3
 
 /* 
