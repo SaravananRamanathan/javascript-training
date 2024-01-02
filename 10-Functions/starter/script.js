8a2b59687c48055c5189485d0682b7d1f9c70f98
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2024-01-02 14:43:55 +0530

    functions section complete

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -203,3 +203,55 @@ GOOD LUCK 😀
 // })();
 
 // (() => console.log('This will never run again.'))();
+
+// Closure stuff
+
+// const secureBooking = function() {
+//   let passengerCount = 0;
+
+//   return function() {
+//     passengerCount++;
+//     console.log(`New count: ${passengerCount} passengers`);
+//   }
+// }
+
+// const booker = secureBooking();
+// booker()
+// booker()
+// booker()
+// console.dir(booker)
+
+// let f;
+
+// const g = function() {
+//   const a = 23;
+//   f = function () {
+//     console.log(a*2);
+//   }
+// }
+// console.log("testing closure.");
+// g();
+// f();
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  console.log('testing');
+  document.querySelector('body').addEventListener('click',function(){
+    header.style.color = header.style.color === 'blue' ? 'red' : 'blue';
+  });
+})();
