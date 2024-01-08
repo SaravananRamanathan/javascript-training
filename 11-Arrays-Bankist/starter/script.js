22ebbfdd70bc024f90d5e868eac83a87d703a1b9
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2024-01-08 10:12:34 +0530

    array section: foreach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -120,3 +120,22 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // console.log(emptyArr.at(-1));
 
 // console.log('srvnn'.at(-1));
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// // for (const movement of movements) {
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0){
+//     console.log(`movement ${i+1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`movement ${i+1}: you withdrew ${Math.abs(movement)}`);
+//   }
+// }
+// console.log("----------FOREACH------");
+// movements.forEach(function(movement, i, arr) {
+//   if (movement > 0){
+//     console.log(`movement ${i+1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`movement ${i+1}: you withdrew ${Math.abs(movement)}`);
+//   }
+// })
